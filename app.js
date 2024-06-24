24ccae560d7289e4e63ff5e164d3249031e45896
unknown <falletkamagate3@gmail.com>
2024-06-24 19:13:40 +0200

    update

--- app.js
+++ app.js
@@ -39,9 +39,9 @@ app.use(function (req, res, next) {
 app.use('/api/user', UserRouter);
 
 // Start the server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+//const PORT = process.env.PORT || 5000;
+//app.listen(PORT, () => {
+  //console.log(`Server is running on port ${PORT}`);
+//});
 
 module.exports = app;
